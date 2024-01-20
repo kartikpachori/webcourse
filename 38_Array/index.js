71afc24d7ad155ad7706b3df4b9de4242bf750bf
kartikpachori <kartikeypachori1610@gmail.com>
2024-01-20 18:02:56 +0530

    readme

--- 38_Array/index.js
+++ 38_Array/index.js
@@ -14,8 +14,8 @@ console.log(arr.join(" and "))
 
 
 
-// let numbers = [1, 2, 3, 4, 5]
+let numbers = [1, 2, 3, 4, 5]
 // numbers.splice(1, 2)
 // numbers.splice(1, 3)
-// numbers.splice(1, 3, 222, 333)
+numbers.splice(1, 3, 222, 333)
 // (4) [1, 222, 333, 5]
