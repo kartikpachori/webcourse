d0f07d3ecb80c157f505d5b32010b336c06db08e
kartikpachori <kartikeypachori1610@gmail.com>
2024-04-22 23:35:05 +0530

    update

--- 59_express/main.js
+++ 59_express/main.js
@@ -4,8 +4,29 @@ const port = 3000
 
 
 //app.get or app.put.app.post app.delete(path,handler)
-app.get('/', (req, res) => {
-    res.send('Hello World23!')
+// app.get('/', (req, res) => {
+//     res.send('Hello World2!')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('About us')
+// })
+
+// app.get('/contact', (req, res) => {
+//     res.send('Contact me!!')
+// })
+
+
+// app.get('/blog', (req, res) => {
+//     res.send('Blogging..')
+// })
+
+app.get('/blog/:slug', (req, res) => {
+    // console.log(req)
+    console.log(req)
+    console.log(req.params)
+    console.log(req.query)
+    res.send(`hello ${req.params.slug}`)
 })
 
 app.listen(port, () => {
